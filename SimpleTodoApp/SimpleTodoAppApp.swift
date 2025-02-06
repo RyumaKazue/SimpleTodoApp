ed205eb2baa8e8b0929d8dd8ff1e44a5cc27aeaa
RyumaKazue <goodane62@gmail.com>
2025-02-06 15:18:48 +0900

    Initial Commit

---
+++ SimpleTodoApp/SimpleTodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTodoAppApp.swift
+//  SimpleTodoApp
+//
+//  Created by apple on 2025/02/06.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
